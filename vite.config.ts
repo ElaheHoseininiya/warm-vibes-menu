2b88d6ca98ab257ba05561af18592508b1019179
elae <elahehoseini99626@gmail.com>
2025-10-25 09:42:34 -0300

    deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig(({ mode }) => ({
-  base: '/warm-vibes-menu/', // ← مهم برای GitHub Pages
+  base: mode === 'development' ? '/' : '/warm-vibes-menu/',
   server: {
-    host: "::",
+    host: '::',
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-}));
+}))
 
